f4e793d83a5aa49c529b0e1babc75b3ebc8feaab
raptorggi <raptorggi@gmail.com>
2018-08-26 13:29:50 +1000

    added minimap render

--- main.js
+++ main.js
@@ -22,18 +22,25 @@ class Point {
   }
 }
 
-//------- END ---------------------------------
+//------------------ END ------------------
 
 class Game {
-  constructor(w, h, canvas, containerId){
-    this.canvas = document.getElementById(canvas);
+  constructor(w, h, cell_w, cell_h, canvas, containerId) {
+    this.canvas_id = canvas;
+    this.container_id = containerId;
+    this.cell_width = cell_w;
+    this.cell_height = cell_h;
+    this.cells_width_count = w;
+    this.cells_height_count = h;
+    this.canvas_width = this.cells_width_count * this.cell_width + 5 + this.cells_width_count * 2;
+    this.canvas_height = this.cells_height_count * this.cell_height;
+    this.map_width = this.cells_width_count * this.cell_width;
+    this.map_height = this.canvas_height;
+    this.loadCanvas();
+    this.canvas = document.getElementById(this.canvas_id);
     this.ctx = this.canvas.getContext("2d");
-    this.width = w;
-    this.height = h;
     this.started = 1;
     this.map = new GameMap(40, 40);
-    this.player_coords = Array(2).fill(0);
-    this.player_coords_new = Array(2).fill(0);
     this.keys = document.getElementById(containerId);
 
     this.keys.addEventListener('keyup', function (event) {
@@ -53,38 +60,41 @@ class Game {
     });
   }
 
+  loadCanvas() {
+    let canvas = document.createElement('canvas'),
+    div = document.getElementById(this.container_id);
+    canvas.id = this.canvas_id;
+    canvas.width = this.canvas_width;
+    canvas.height = this.canvas_height;
+    canvas.style.position = "absolute";
+    canvas.style.border = "1px solid";
+    div.appendChild(canvas);
+  }
+
   stop() {
     this.started = 0;
   }
 
   drawMainScreen() {
     this.clearMainScreen();
-    let x = this.map.player_coords[0];
-    let y = this.map.player;
-    for (let i = this.map.player_coords[0] - 20; i < 30; i++) {
-      for (let j = 0; j < 40; j++) {
-        if (this.game_field[i][j] != null) {
-          this.game_field[i][j].draw(j, i, this.ctx);
-        }
-      }
-    }
-
     this.ctx.fillStyle = "#dd99ff";
-    this.ctx.fillRect(this.width, this.height, 5, this.height);
+    this.ctx.fillRect(this.map_width, 0, 5, this.canvas_height);
     this.drawMinimap();
   }
 
   drawMinimap() {
     for (let y = 0; y < this.map.height; y++) {
       for (let x = 0; x < this.map.width; x++) {
-        this.map.game_field[y][x].drawOnMinimap(this.width + 5 + x, y);
+        if (this.map.game_field[y][x] != null) {
+          this.map.game_field[y][x].drawOnMinimap(this.map_width + 5 + x * 2, y * 2, this.ctx);
+        }
       }
     }
   }
 
   clearMainScreen() {
     this.ctx.fillStyle = "#000000";
-    this.ctx.fillRect(0, 0, this.width, this.height);
+    this.ctx.fillRect(0, 0, this.canvas_width, this.canvas_height);
   }
 
   isStarted() {
@@ -129,32 +139,28 @@ class GameMap {
     this.width = w;
     this.height = h;
     this.game_field = Array(this.height).fill(null).map(()=>Array(this.width).fill(null)); // map W x H
-    this.player_coords = new Point(randomInteger(0, this.height - 1), randomInteger(0, this.width - 1));
+    this.player_coords = new Point(Math.abs(randomInteger(0, this.height - 1)), Math.abs(randomInteger(0, this.width - 1)));
   }
 
   isInMap(coords) {
-    if (coords.x > 0 && coords.y > 0 && coords.x < this.width && coords.y < this.height) {
+    if (coords.x >= 0 && coords.y >= 0 && coords.x < this.width && coords.y < this.height) {
      return  true;
-     console.log(true)
     }
     return false;
-    console.log(false)
   }
 
   generateGameField() {
-    console.log(1);
     let map = Array(this.height).fill(1).map(()=>Array(this.width).fill(1)); // map W x H
     let dig_position = this.player_coords.copy();
     let dig_position_new = dig_position.copy();
-    console.log(2);
-    let cells_to_open = Math.round(this.height * this.width * 0.01);
-    console.log(cells_to_open);
+    let cells_to_open = Math.round(this.height * this.width * 0.1);
     while (cells_to_open > 0) {
-      switch (randomInteger(0, 3)) {
-        case 1: dig_position_new[0] += 1;
-        case 2: dig_position_new[0] -= 1;
-        case 3: dig_position_new[1] += 1;
-        case 0: dig_position_new[1] -= 1;
+      let t = Math.abs(randomInteger(0, 3));
+      switch (t) {
+        case 1: dig_position_new.y += 1;
+        case 2: dig_position_new.y -= 1;
+        case 3: dig_position_new.x += 1;
+        case 0: dig_position_new.x -= 1;
       }
       if (this.isInMap(dig_position_new)){ 
         if (map[dig_position_new.y][dig_position_new.x] != 0 ) {
@@ -162,61 +168,77 @@ class GameMap {
           map[dig_position.y][dig_position.x] = 0;
           cells_to_open -= 1;
         }
+        else {
+          dig_position = dig_position_new.copy();
+        }
       }
       else {
         dig_position = this.player_coords.copy();
       }
       dig_position_new = dig_position.copy();
     }
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (map[y][x] == 1) {
+          this.game_field[y][x] = new Wall(game.cell_width, game.cell_height);
+        }
+      }
+    }
+    this.game_field[this.player_coords.y][this.player_coords.x] = new Player(game.cell_width, game.cell_height);
   }
+
 } 
 
 class Object {
-  constructor() {
-    this.width = 20;
-    this.height = 20;
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    this.color = "#000000";
   }
 
 }
 
 class Wall extends Object {
-  constructor() {
+  constructor(w, h) {
     super();
+    this.width = w;
+    this.height = h;
+    this.color = "#d9d9d9";
   }
 
   drawOnMain(posX, posY, ctx) {
     let x = posX * this.width;
     let y = posY * this.height;
-    ctx.fillStyle = '#d9d9d9';
+    ctx.fillStyle = this.color;
     ctx.fillRect(x, y, this.width - 1, this.height - 1);
   }
 
   drawOnMinimap(posX, posY, ctx) {
-    ctx.fillStyle = '#d9d9d9';
-    ctx.fillRect(posX, posY, 1, 1);
+    ctx.fillStyle = this.color;
+    ctx.fillRect(posX, posY, 2, 2);
   }
 }
 
 class Player extends Object {
-  constructor() {
+  constructor(w, h) {
     super();
+    this.width = w;
+    this.height = h;
+    this.color = "#0080ff";
   }
 
   drawOnMain(posX, posY, ctx) {
     let x = posX * this.width + 3;
     let y = posY * this.height + 3;
-    ctx.fillStyle = '#0080ff';
+    ctx.fillStyle = this.color;
     ctx.fillRect(x, y, this.width - 6, this.height - 6);
   }
 
   drawOnMinimap(posX, posY, ctx) {
-    ctx.fillStyle = '#0080ff';
-    ctx.fillRect(posX, posY, 1, 1);
+    ctx.fillStyle = this.color;
+    ctx.fillRect(posX, posY, 2, 2);
   }
 }
-
-let game = new Game(820, 620, 'canvas', 'game');
-let tick_interval = 100;
 
 function move() {
   idTimer = setInterval('mainLoop();', tick_interval);
@@ -224,8 +246,8 @@ function move() {
 
 function mainLoop() {
   if (game.isStarted()) {
-    game.playerMove();
-    game.draw();
+    // game.playerMove();
+    game.drawMainScreen();
   }
   else {
     clearInterval(idTimer);
@@ -233,5 +255,7 @@ function mainLoop() {
 }
 
 function init() {
+  window.game = new Game(31, 31, 15, 15, 'canvas', 'game');
+  window.tick_interval = 100;
   game.generateMap();
 }
